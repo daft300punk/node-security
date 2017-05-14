c0f41f15c7461cd4fc626050c40ec55076804b52
Abhilash Singh <daft300punk@gmail.com>
2017-05-14 10:02:49 +0530

    Authentication actions start

---
+++ shared/actions/authenticationActions.js
@@ -0,0 +1,60 @@
+"use strict";
+
+import * as types from "../constants/actionTypes";
+import authenticationApi from "../api-services/authenticationApi";
+import { routerActions } from "react-router-redux;
+
+function userDateUpdate(user) {
+  return {
+    type: types.USERDATE_UPDATED,
+    user
+  }
+}
+
+function userAuthenticationRequested() {
+  return {
+    type: types.LOGIN_REQUEST,
+    isAuthenticated: false,
+    isRequested: true,
+  }
+}
+
+function userAuthenticationSuccess(user) {
+  return {
+    type: types.LOGIN_SUCCESS,
+    isRequested: false,
+    isAuthenticated: true,
+    user
+  }
+}
+
+function userAuthenticationFailure(user) {
+  return {
+    type: types.LOGIN_FAILURE,
+    isAuthenticated: false,
+    isRequested: false,
+    error
+  }
+}
+
+function userLogoutRequested() {
+  return {
+    type: types.LOGOUT_REQUEST,
+    isRequested: true,
+  }
+}
+
+function userLogoutSuccessful() {
+  return {
+    type: types.LOGOUT_SUCCESS,
+    isRequested: false,
+  }
+}
+
+function userLogoutFailure(errorMessage) {
+  return {
+    type: types.LOGOUT_FAILURE,
+    isRequested: true,
+    errorMessage
+  }
+}
