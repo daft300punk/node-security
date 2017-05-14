27ec623826beae342341e9fd5f0cd5279005f3d3
Abhilash Singh <daft300punk@gmail.com>
2017-05-14 10:05:54 +0530

    Action creators complete

--- shared/actions/authenticationActions.js
+++ shared/actions/authenticationActions.js
@@ -58,3 +58,30 @@ function userLogoutFailure(errorMessage) {
     errorMessage
   }
 }
+
+function userRegistrationRequested() {
+  return {
+    type: types.REGISTRATION_REQUEST,
+    isRequested: true,
+    isRegistered: false
+  }
+}
+
+function userRegistrationSuccess(message) {
+  return {
+    type: types.REGISTRATION_SUCCESS,
+    isRequested: false,
+    isRegistered: true,
+    message
+  }
+}
+
+function userRegistrationFailure(errors) {
+  return {
+    type: types.REGISTRATION_FAILURE,
+    isRegistered: false,
+    isRequested: false,
+    errors
+  }
+}
+
