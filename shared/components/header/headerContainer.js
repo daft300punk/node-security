beb2670855c8512e5d81548536d2f4f74034c4b2
Abhilash Singh <daft300punk@gmail.com>
2017-05-14 15:04:46 +0530

    Header container

---
+++ shared/components/header/headerContainer.js
@@ -0,0 +1,62 @@
+"use strict";
+
+import React, {PropTypes, Component} from "react";
+import {connect} from "react-redux";
+import HeaderSiteNav from "./headerSiteNavComponent";
+import HeaderUserAuthNav from "./headerUserAuthNavComponent";
+import HeaderUserProfile from "./headerUserProfileComponent";
+import {bindActionCreators} from "redux";
+import * as authActions from "../../actions/authenticationActions";
+import _ from "underscore";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user;
+    };
+
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    const {authActions} = this.props;
+    event.preventDefault();
+    authActions.logout();
+  }
+
+  render() {
+    const {user} = this.props;
+    if(!_.isEmpty(user)) {
+      user.identity = (user.firstName && user.lastName) ? `${user.firstName} ${user.lastName}` :
+        user.firstName ? `${user.firstName}` :
+          user.lastName ? `${user.lastName}` :
+            user.username ? `${user.username}` :
+              undefined
+    }
+
+    return (
+      <nav className="header-nav-container">
+        <HeaderSiteNav />
+        {!_.isEmpty(user) && <HeaderUserProfile user={user} logout={this.logout} /> || <HeaderUserAuthNav />}
+      </nav>
+    )
+  }
+}
+
+Header.propTypes = {
+  user: Proptypes.object,
+}
+
+const mapStateToProps = (state) => {
+  const {userDataState} = state;
+  return {user: userDataState.user};
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    authActions: bindActionCreators(authActions, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, null, {pure: false})(Header);
